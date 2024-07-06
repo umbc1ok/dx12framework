a452ee593d33406ecab68d0a750d6030e1ea626d
umbc1ok <umbcio@gmail.com>
2024-07-06 03:07:22 +0200

    Utils: Add swap_and_erase and guid functions

---
+++ src/utils/Utils.h
@@ -0,0 +1,56 @@
+#include <random>
+#include <sstream>
+
+namespace olej_utils
+{
+    template<typename T>
+    void swap_and_erase(std::vector<T>& vector, T element)
+    {
+        if (auto const it = std::ranges::find(vector, element); it != vector.end())
+        {
+            // NOTE: Swap with last and pop to avoid shifting other elements.
+            std::swap(vector.at(it - vector.begin()), vector.at(vector.size() - 1));
+            vector.pop_back();
+        }
+    }
+
+    inline unsigned char random_char()
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 255);
+        return static_cast<unsigned char>(dis(gen));
+    }
+
+    inline std::string generate_hex(unsigned int const length)
+    {
+        std::stringstream ss;
+        for (unsigned int i = 0; i < length; ++i)
+        {
+            auto const random_character = random_char();
+            std::stringstream hexstream;
+            hexstream << std::hex << static_cast<int>(random_character);
+            auto hex = hexstream.str();
+            ss << (hex.length() < 2 ? '0' + hex : hex);
+        }
+        return ss.str();
+    }
+
+    inline std::string generate_guid()
+    {
+        std::string result;
+
+        unsigned int constexpr guid_lengths[] = { 8, 4, 4, 4, 12 };
+
+        for (unsigned int i = 0; i < 5; ++i)
+        {
+            result += generate_hex(guid_lengths[i]);
+        }
+
+        return result;
+    }
+
+
+
+
+}
