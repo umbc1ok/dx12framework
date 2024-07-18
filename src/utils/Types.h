472585fa4db8a996fa38381ba1060b7b7559e1a4
umbc1ok <umbcio@gmail.com>
2024-07-18 19:07:20 +0200

    Utils: Add murmur hash

--- src/utils/Types.h
+++ src/utils/Types.h
@@ -3,8 +3,8 @@
 //using u64 = std::uint64_t;
 using u32 = unsigned int;
 //using u16 = std::uint16_t;
-//using u8 = std::uint8_t;
+using u8 = std::uint8_t;
 //using i64 = std::int64_t;
 using i32 = int;
 //using i16 = std::int16_t;
-//using i8 = std::int8_t;
+using i8 = std::int8_t;
