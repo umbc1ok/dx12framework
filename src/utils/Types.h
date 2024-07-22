4647c8ae4f05e5dbe80092b4a4a64d388316c431
umbc1ok <umbcio@gmail.com>
2024-07-22 19:00:39 +0200

    Types: Add u16 as std::uint16_t

--- src/utils/Types.h
+++ src/utils/Types.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <cstdint>
 
 //using u64 = std::uint64_t;
 using u32 = unsigned int;
-//using u16 = std::uint16_t;
+using u16 = std::uint16_t;
 using u8 = std::uint8_t;
 //using i64 = std::int64_t;
 using i32 = int;
