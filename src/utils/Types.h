b0337eac8272962d601752a841bca757565bf510
umbc1ok <umbcio@gmail.com>
2024-07-23 00:49:42 +0200

    Types: Use native types instead of wrappers

--- src/utils/Types.h
+++ src/utils/Types.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <cstdint>
 
-//using u64 = std::uint64_t;
+using u64 = unsigned long long;
 using u32 = unsigned int;
-using u16 = std::uint16_t;
-using u8 = std::uint8_t;
-//using i64 = std::int64_t;
+using u16 = unsigned short;
+using u8 = unsigned char;
+using i64 = long long;
 using i32 = int;
-//using i16 = std::int16_t;
-using i8 = std::int8_t;
+using i16 = short;
+using i8 = signed char;
