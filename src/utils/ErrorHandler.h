ad2b524c510294d13493616ba0767efa5f2fc62e
Hubert Olejnik <umbcio@gmail.com>
2024-09-11 19:05:34 +0200

    ErrorHandler: Add GetDeviceRemovedReason() on fail

--- src/utils/ErrorHandler.h
+++ src/utils/ErrorHandler.h
@@ -3,11 +3,14 @@
 #include <cassert>
 #include <Windows.h>
 
+#include "Renderer.h"
+
 // TODO: Check if it actually stops the program
 inline void AssertFailed(HRESULT hr)
 {
     if (FAILED(hr) == true)
     {
-        printf("Error: %d\n", hr);
+        printf("Error: %ld\n", hr);
+        FAILED(Renderer::get_instance()->get_device()->GetDeviceRemovedReason()) == true;
     }
 }
