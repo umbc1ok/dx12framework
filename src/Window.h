e135a60dcb51bcee39047f818f8904da6af8fb69
Hubert Olejnik <umbcio@gmail.com>
2024-10-13 15:30:56 +0200

    Window: Add cursor mode switching

--- src/Window.h
+++ src/Window.h
@@ -18,13 +18,14 @@ public:
 
     static HWND get_hwnd() { return hwnd; }
     static void update_window_name(std::string const& name);
-
+    static bool get_cursor_visible() { return cursor_visible; }
 private:
     static LRESULT WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    static void change_mouse_mode();
 
     static WNDCLASSEXW wc;
     static HWND hwnd;
-
+    inline static bool cursor_visible = true;
     static Window* m_instance;
 
 };
