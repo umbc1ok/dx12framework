2700d7496dd48cb891097c980d8634d6b9214fdd
Hubert Olejnik <umbcio@gmail.com>
2025-01-05 01:09:57 +0100

    Serialization: Add vector and mesh serialization

---
+++ src/Serialization/MeshSerializer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+#include "MeshletStructs.h"
+#include "DX12Wrappers/Vertex.h"
+#include "DXMeshletGenerator/D3D12MeshletGenerator.h"
+#include "types/VectorSerializer.h"
+#include "utils/maths.h"
+
+
+namespace serializers
+{
+    bool serializeMesh(
+        const std::vector<Vertex>& vertices,
+        const std::vector<uint32_t>& indices,
+        const std::vector<Meshlet>& meshlets,
+        const std::vector<uint32_t>& meshletTriangles,
+        const std::vector<uint32_t>& attributes,
+        const std::vector<hlsl::float3>& positions,
+        const std::vector<hlsl::float3>& normals,
+        const std::vector<hlsl::float2>& UVs,
+        int32_t MeshletMaxVerts,
+        int32_t MeshletMaxPrims,
+        MeshletizerType type,
+        const std::string& fileName);
+
+    void deserializeMesh(
+         std::vector<Vertex>& vertices,
+         std::vector<uint32_t>& indices,
+         std::vector<Meshlet>& meshlets,
+         std::vector<uint32_t>& meshletTriangles,
+         std::vector<uint32_t>& attributes,
+         std::vector<hlsl::float3>& positions,
+         std::vector<hlsl::float3>& normals,
+         std::vector<hlsl::float2>& UVs,
+        int32_t MeshletMaxVerts,
+        int32_t MeshletMaxPrims,
+        MeshletizerType& type,
+        const std::string& fileName);
+
+}
