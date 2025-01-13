58076a5dd72f87417092f611cc4675296a46d890
Hubert Olejnik <umbcio@gmail.com>
2025-01-13 19:28:45 +0100

    BoundingSphere: Cleanup code/comments

--- src/GreedyMeshletizer/boundingSphereMeshletizer.cpp
+++ src/GreedyMeshletizer/boundingSphereMeshletizer.cpp
@@ -70,9 +70,6 @@ namespace meshletizers::boundingSphere
         PrimitiveCache cache(maxVerts, maxPrims);
         cache.reset();
 
-
-        //std::sort(vertsVector.begin(), vertsVector.end(), std::bind(compareVerts, std::placeholders::_1, std::placeholders::_2, vertexBuffer));
-
         for (int i = 0; i < vertsVector.size();)
         {
             MeshletizerVertex* vert = vertsVector[i];
@@ -131,10 +128,6 @@ namespace meshletizers::boundingSphere
                     }
                     else 
                     {
-                        //TODO TURN THIS IN TO ONE THINK THAT ALWAYS RUNS
-                        // LIKE MAKE SURE THAT THE VERTEX furtherst away from center is used for new radius 
-                        // or calculate three new radius and use the biggest one
-                        // afterwards check the added radius by adding triangle to the cluster
                         float newRadius = 0.5 * (radius + hlsl::length(center - tri->vertices[newVert]->position));
                     }
 
@@ -166,13 +159,6 @@ namespace meshletizers::boundingSphere
                 if (bestTri == nullptr)
                 {
                     ++i;
-                    // here we finalize current meshlet when we need to enforce locality
-                    //if (cache.numPrims != 0) {
-                    //	meshlets.push_back(cache);
-                    //	currentVerts.clear();
-                    //	cache.reset();
-                    //}
-
                     continue;
                 }
             }
@@ -197,7 +183,7 @@ namespace meshletizers::boundingSphere
             }
 
             // If full pack and restart restart
-            //add triangle to cache
+            // add triangle to cache
             if (cache.cannotInsert(candidateIndices, maxVerts, maxPrims)) {
                 // we run out of verts but could push prims more so we do a pass of prims here to see if we can maximize 
                 // so we run through all triangles to see if the meshlet already has the required verts
@@ -237,7 +223,6 @@ namespace meshletizers::boundingSphere
             ++usedVerts[candidateIndices[1]];
             ++usedVerts[candidateIndices[2]];
 
-            //if (indexVertexMap[i]->neighbours.size() == usedVerts[indexVertexMap[i]->index]) ++i;
         }
 
         // add remaining triangles to a meshlet
@@ -245,164 +230,6 @@ namespace meshletizers::boundingSphere
             addMeshlet(meshlets, uniqueVertexIndices, packedPrimitiveIndices, cache);
             cache.reset();
         }
-
-
-
-
-
-
-
-
-        //assert(MAX_VERTEX_COUNT_LIMIT == maxVerts);
-        //assert(MAX_PRIMITIVE_COUNT_LIMIT == maxPrims);
-        //std::unordered_map<unsigned int, unsigned char> usedVertices;
-
-        //Border border = Border();
-        //border.addTriangle(triangles[0]);
-        //PrimitiveCache cache;
-        //cache.reset();
-
-        //Triangle* bestTriangle = nullptr;
-        //float radius = 0.0f;
-        //hlsl::float3 center = hlsl::float3(0.0f, 0.0f, 0.0f);
-
-        //for(int i = 0; i < vertsVector.size(); i++)
-        //{
-        //    MeshletizerVertex* vertex = vertsVector[i];
-        //    //if (usedVertices.contains(vertex->index))
-        //    //    continue;
-
-        //    bestTriangle = nullptr;
-        //    // whatever that means
-        //    int32_t newVertex = -1;
-        //    float newRadius = FLT_MAX;
-        //    float bestNewRadius = FLT_MAX - 1;
-        //    // The vertexScore for a triangle increases for each vertex that is already in
-        //    // the meshlet and if it is considered a triangle island.
-        //    // via: da paper
-        //    uint32_t bestVertexScore = 0;
-        //    // populate the border
-        //    for (auto* t : vertex->neighbours)
-        //    {
-        //        if (t->usedFlag)
-        //            continue;
-        //        border.addTriangle(t);
-        //    }
-
-        //    // establish which triangle is best
-        //    for (uint32_t triangleIndex = 0; triangleIndex < border.size(); triangleIndex++)
-        //    {
-        //        // this is logical, that this should be here, why does the paper have it in the previous scope????
-        //        // unless they forgot to reset it to 0 somewhere else
-        //        uint32_t vertexScore = 0;
-        //        auto* tri = border.at(triangleIndex);
-        //        if (tri->usedFlag)
-        //            continue;
-
-        //        for (uint32_t j = 0; j < 3; j++)
-        //        {
-        //            if (cache.isInserted(tri->vertices[j]->index))
-        //                vertexScore++;
-        //            else
-        //                newVertex = tri->vertices[j]->index;
-        //        }
-
-        //        if (vertexScore == 3)
-        //            newRadius = radius;
-        //        else if (vertexScore == 1)
-        //            continue;
-        //        else if (newVertex > -1)
-        //            newRadius = 0.5f * (radius + hlsl::length(center - vertsVector[newVertex]->position));
-
-        //        uint32_t trianglesInMeshlet = 0;
-
-        //        for (Triangle* t : tri->neighbours)
-        //        {
-        //            // Pseudo code: if tri is in Meshlet
-        //            // Logically this should work, if I understood the paper correctly.
-        //            if (t->usedFlag)
-        //                trianglesInMeshlet++;
-        //        }
-
-        //        // if all triangles's neighbors are in the meshlet, it should be added
-        //        if (tri->neighbours.size() == trianglesInMeshlet)
-        //            vertexScore++;
-
-        //        if (vertexScore >= bestVertexScore || newRadius <= bestNewRadius)
-        //        {
-        //            bestVertexScore = vertexScore;
-        //            bestNewRadius = newRadius;
-        //            bestTriangle = tri;
-        //        }
-        //    }
-
-        //    if (bestTriangle == nullptr)
-        //    {
-        //        for (Triangle* t : vertex->neighbours)
-        //        {
-        //            if (t->usedFlag)
-        //                continue;
-        //            bestTriangle = t;
-        //            center = (bestTriangle->vertices[0]->position + bestTriangle->vertices[1]->position + bestTriangle->vertices[2]->position) / 3.0f;
-        //            float tempRadius = max(hlsl::length(center - bestTriangle->vertices[0]->position), hlsl::length(center - bestTriangle->vertices[1]->position));
-        //            tempRadius = max(tempRadius, hlsl::length(center - bestTriangle->vertices[2]->position));
-        //            bestNewRadius = tempRadius;
-        //        }
-        //    }
-
-        //    // if we have a nullptr here it means the vertex doesn't have any unused neighbours
-        //    if (bestTriangle == nullptr)
-        //    {
-        //        //i++; // not sure why this is in the pseudo code, most likely a mistake
-        //        continue;
-        //    }
-        //    radius = bestNewRadius;
-        //    center = vertsVector[newVertex]->position + (radius / (FLT_EPSILON + hlsl::length(center - vertsVector[newVertex]->position))) * (center - vertsVector[newVertex]->position);
-
-        //    std::vector<uint32_t> bestTriangleIndices = { bestTriangle->vertices[0]->index, bestTriangle->vertices[1]->index , bestTriangle->vertices[2]->index };
-        //    if(cache.cannotInsert(bestTriangleIndices.data(), maxVerts, maxPrims))
-        //    {
-        //        if(cache.numPrimitives < maxPrims)
-        //        {
-        //            for(auto* t : border.triangles)
-        //            {
-        //                if(cache.isInserted(t->vertices[0]->index) && cache.isInserted(t->vertices[1]->index) && cache.isInserted(t->vertices[2]->index))
-        //                {
-        //                    std::vector<uint32_t> t_indices = { t->vertices[0]->index,  t->vertices[1]->index,  t->vertices[2]->index };
-        //                    if(!cache.cannotInsert(t_indices.data(), maxVerts, maxPrims))
-        //                    {
-        //                        t->usedFlag = true;
-        //                        cache.insert(t_indices.data());
-        //                        border.removeTriangle(t);
-
-        //                        for (uint32_t j = 0; j < 3; j++)
-        //                        {
-        //                            uint32_t vertexIndex = t->vertices[j]->index;
-        //                            // todo: this gives better results when commented out
-        //                            //usedVertices[vertexIndex] = true;
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
-        //        addMeshlet(meshlets, uniqueVertexIndices, packedPrimitiveIndices, cache);
-        //        // next two are my contribution
-        //        radius = 0;
-        //        center = hlsl::float3(0.0f, 0.0f, 0.0f);
-        //        cache.reset();
-        //        border.clear();
-        //        continue;
-        //    }
-        //    cache.insert(bestTriangleIndices.data());
-        //    for (uint32_t j = 0; j < 3; j++)
-        //    {
-        //        uint32_t vertexIndex = bestTriangle->vertices[j]->index;
-        //        // todo: this gives better results when commented out
-        //        //usedVertices[vertexIndex] = true;
-        //    }
-        //    border.removeTriangle(bestTriangle);
-        //    bestTriangle->usedFlag = true;
-        //}
 
     }
 }
